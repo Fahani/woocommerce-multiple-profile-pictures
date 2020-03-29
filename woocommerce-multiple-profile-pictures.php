75871c7d27d5fdea81a25b00129c6482e337ce96
Nico <nicolas.gonzalez@techpump.com>
2020-03-29 12:51:30 +0200

    Creating main plugin file with general plugin information

---
+++ woocommerce-multiple-profile-pictures.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: WooCommerce Multiple Profile Pictures
+ * Plugin URI: https://github.com/Fahani/woocommerce-multiple-profile-pictures
+ * Description: Allows a customer to have <strong>multiple profile pictures</strong> to switch between them. The <strong>profile picture will be save when placing an order</strong>.
+ * Version: 1.0.0
+ * Author: Nicolás González
+ * Author URI: https://github.com/Fahani
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * @package WC-Multiple-Profile-Pictures
+ *
+ */
