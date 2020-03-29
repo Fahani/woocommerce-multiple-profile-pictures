680e3bc74f71083b3d20e7f7885f5e5aae17c911
Nico <nicolas.gonzalez@techpump.com>
2020-03-29 13:08:05 +0200

    - Adding security checks at beginning - Adding const for minimum plugin requirements - Using Singleton Patter to load main class one time

--- woocommerce-multiple-profile-pictures.php
+++ woocommerce-multiple-profile-pictures.php
@@ -12,3 +12,50 @@
  * @package WC-Multiple-Profile-Pictures
  *
  */
+
+defined( 'ABSPATH' ) or die( 'This is not what you are looking for' );
+
+/**
+ * Plugin main class
+ *
+ * @since 1.0.0
+ */
+class MultipleProfilePictures {
+
+	/** the plugin name, for displaying notices */
+	const PLUGIN_NAME = 'WooCommerce Multiple Profile Pictures';
+
+	/** minimum PHP version required by this plugin */
+	const MINIMUM_PHP_VERSION = '7.1.0';
+
+	/** minimum WooCommerce version required by this plugin */
+	//TODO Checks real requirements at the end
+	const MINIMUM_WC_VERSION = '3.0.9';
+
+	/** minimum WordPress version required by this plugin */
+	//TODO Checks real requirements at the end
+	const MINIMUM_WP_VERSION = '4.4';
+
+	/** @var MultipleProfilePictures */
+	protected static $instance;
+
+	public function __construct() {
+
+	}
+
+	/**
+	 * Ensures only one instance is set.
+	 * @since 1.0.0
+	 * @return MultipleProfilePictures
+	 */
+	public static function instance(): MultipleProfilePictures {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
+
+// Fire it up! :)
+$myPlugin = MultipleProfilePictures::instance();
