55d87403055bc3e40a9248f084b0b1baa1544994
Nico <nicolas.gonzalez@techpump.com>
2020-05-20 23:52:33 +0200

    Removing the info from user_meta and post_meta

--- uninstall.php
+++ uninstall.php
@@ -1,20 +1,45 @@
 <?php
-
-
 /**
  * Trigger this file when uninstalling the plugin
  *
  */
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	die('This is not what you are looking for');
+	die( 'This is not what you are looking for' );
 }
 
-global $wpdb;
+/**
+ * Removes the main_picture meta from orders purchased by users with main picture
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function delete_meta_from_orders() {
+	delete_post_meta_by_key( 'main_picture' );
+}
 
-$wpdb->query( "DROP TABLE `{$wpdb->prefix}woocommerce_mpp_user_picture`" );
-$wpdb->query( "DROP TABLE `{$wpdb->prefix}woocommerce_mpp_order_picture`" );
+/**
+ * Removes the main_picture meta from users when they selected a main picture
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function delete_meta_from_users() {
+	$users_with_main_picture = get_users( [ 'meta_key' => 'main_picture' ] );
+	foreach ( $users_with_main_picture as $user ) {
+		delete_user_meta( $user->ID, 'main_picture' );
+	}
+}
 
+/**
+ * Removes the wp_option that stores the number of max pictures an user can upload
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function delete_wmpp_settings() {
+	delete_option( 'max_profile_pictures' );
+}
 
 /**
  * Deletes folder, files and sub folders
@@ -35,4 +60,7 @@ function deleteAll( $dir ) {
 	rmdir( $dir );
 }
 
-deleteAll(wp_upload_dir()['basedir'] . '/wmpp');
+deleteAll( wp_upload_dir()['basedir'] . '/wmpp' );
+delete_meta_from_orders();
+delete_meta_from_users();
+delete_wmpp_settings();
